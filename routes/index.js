928a9ead937a07eca5212a968c566a4805e03996
hemi333 <71807433+hemi333@users.noreply.github.com>
2022-10-31 16:48:33 +0900

    add Balances, Clients - Read
    add Balances, Clients - Read

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const balanceRouter = require("./balance");
 const clientsRouter = require("./clients");
 
-router.use("/admin", balanceRouter);
-router.use("/main", clientsRouter);
+router.use("/balances", balanceRouter);
+router.use("/clients", clientsRouter);
 
 module.exports = router;
